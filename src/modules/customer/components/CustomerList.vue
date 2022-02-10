4943fb3503a51462aeedd18a0ec33f0cf0bc2b2b
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-02-10 15:08:05 -0600

    Vista funcional

--- src/modules/customer/components/CustomerList.vue
+++ src/modules/customer/components/CustomerList.vue
@@ -1,5 +1,13 @@
 <template>
     <h1>Este es el customer list</h1>
+    <ol>
+        <li>Customer Juan</li>
+        <li>Customer Luis</li>
+        <li>Customer Mario</li>
+        <li>Customer Enrique</li>
+        <li>Customer Martin</li>
+        <li>Customer Ernesto</li>
+    </ol>
 </template>
 
 <script>
