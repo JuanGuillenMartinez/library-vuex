0173ca589893617950ca8a4265f1b2b9c8e45ecb
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-02-11 09:41:58 -0600

    Bootstrap implementado por medio de npm

--- src/modules/customer/components/CustomerList.vue
+++ src/modules/customer/components/CustomerList.vue
@@ -1,19 +1,7 @@
 <template>
-    <div v-for="customer in customers" :key="customer.id" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="toast-header">
-            <img src="@/assets/logo.png" class="rounded me-2" alt="..." />
-            <strong class="me-auto">{{ customer.name }}</strong>
-            <small>{{ customer.created_at }}</small>
-            <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="toast"
-                aria-label="Close"
-            ></button>
-        </div>
-        <div class="toast-body">Hello, world! This is a toast message.</div>
+    <div v-for="customer in customers" :key="customer.id" class="card">
+        <div class="card-body">{{ customer.name }}</div>
     </div>
-    <h1>hola</h1>
 </template>
 
 <script>
