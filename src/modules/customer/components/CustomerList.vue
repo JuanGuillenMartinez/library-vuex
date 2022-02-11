e59cd488044a5e3cc419772b4fe244802725a9a7
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-02-11 09:24:57 -0600

    Eliminado bootstrap del proyecto

--- src/modules/customer/components/CustomerList.vue
+++ src/modules/customer/components/CustomerList.vue
@@ -1,22 +1,34 @@
 <template>
-    <ol>
-        <li v-for="customer in customers" :key="customer.id">{{ customer.name }}</li>
-    </ol>
+    <div v-for="customer in customers" :key="customer.id" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-header">
+            <img src="@/assets/logo.png" class="rounded me-2" alt="..." />
+            <strong class="me-auto">{{ customer.name }}</strong>
+            <small>{{ customer.created_at }}</small>
+            <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="toast"
+                aria-label="Close"
+            ></button>
+        </div>
+        <div class="toast-body">Hello, world! This is a toast message.</div>
+    </div>
+    <h1>hola</h1>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex';
+import { mapActions, mapGetters } from "vuex";
 
 export default {
     computed: {
-        ...mapGetters('CustomerStore', ['customers'])
+        ...mapGetters("CustomerStore", ["customers"]),
     },
     methods: {
-        ...mapActions('CustomerStore', [ 'fetchCustomerList' ]),
+        ...mapActions("CustomerStore", ["fetchCustomerList"]),
     },
     created() {
-        this.fetchCustomerList()
-    }
+        this.fetchCustomerList();
+    },
 };
 </script>
 
